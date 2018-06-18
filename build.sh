af85e4819cd5559178e565e213f792b686f6d1d0
Logan Darklock <logandark@allotropelabs.com>
2018-06-18 08:51:17 -0700

    Rename "doc" to "docs" for GitHub Pages

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-rm -rf dist doc
+rm -rf dist ./docs
 tsc
-typedoc --exclude dist --out doc --readme none --theme markdown --mdEngine github
+typedoc --exclude dist --out ./docs --readme none --theme markdown --mdEngine github
